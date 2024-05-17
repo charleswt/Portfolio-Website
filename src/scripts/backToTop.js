d31928903fec02cefc64c3548b11c81b39d7d14b
ct <charleswtiffany@gmail.com>
2024-05-16 21:24:41 -0700

    added scroll to top

---
+++ src/scripts/backToTop.js
@@ -0,0 +1,21 @@
+const topButton = document.querySelector('.top');
+
+function checkPosition() {
+    let position = window.scrollY
+    if (window.scrollY >=31 && topButton.style.display !== "block") {
+        topButton.style.display = 'block';
+        console.log('its working')
+    } else if (window.scrollY <=30){
+        topButton.style.display = 'none';
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+    });
+}
+
+topButton.addEventListener('click', scrollToTop);
+document.addEventListener('scroll', checkPosition);
+checkPosition();
