5bfb6cdaaeb5742c85436ba7c7923a15abb01087
ct <charleswtiffany@gmail.com>
2024-09-25 15:21:37 -0700

    added naviagtion functionality to projects

--- src/scripts/scroll.js
+++ src/scripts/scroll.js
@@ -1,23 +1,68 @@
-const topButton = document.querySelector('.top');
 const skillsSlide = document.querySelector('.skills-slide');
 
+const project1 = document.getElementById('project-1');
+const project2 = document.getElementById('project-2');
+const project3 = document.getElementById('project-3');
+const project4 = document.getElementById('project-4');
+const project5 = document.getElementById('project-5');
+const project6 = document.getElementById('project-6');
 
-function checkPosition() {
-    if (window.scrollY >=31 && topButton.style.display !== "block") {
-        topButton.style.display = 'block';
-    } else if (window.scrollY <= 30){
-        topButton.style.display = 'none';
+const projectUl = document.getElementsByClassName("project-ul-position")
+
+const ids = [project1, project2, project3, project4, project5, project6]
+
+let position = JSON.parse(localStorage.getItem("pos")) ?? 0;
+
+document.getElementById("increment-btn")
+.addEventListener("click", ()=>{
+    position === 5? 
+    position = 0:
+    position++
+
+    classManager(position)
+})
+
+document.getElementById("decrement-btn")
+.addEventListener("click", ()=>{
+    position === 0? 
+    position = 5:
+    position--
+
+    classManager(position)
+})
+
+const classManager = (pos)=>{
+    let zIndex = 10
+    ids.forEach((proj, index)=>{
+        if(index === pos){
+            proj.classList.remove("projects-tilt-right")
+            proj.classList.remove("projects-tilt-left")
+            proj.style.zIndex = 16
+        }
+        if(index > pos){
+            proj.style.zIndex = zIndex--
+            proj.classList.add("projects-tilt-left")
+            proj.classList.remove("projects-tilt-right")
+        } else if (index < pos){
+            proj.style.zIndex = zIndex++
+            proj.classList.add("projects-tilt-right")
+            proj.classList.remove("projects-tilt-left")
+        }
+        localStorage.setItem("pos", JSON.stringify(pos))
+        })
+}
+
+ids.forEach((proj, index)=>{
+    if(index > position){
+        proj.classList.add("projects-tilt-left")
+    } else if (index < position){
+        proj.classList.add("projects-tilt-right")
     }
+    })
+
+if(window.scrollY > 190){
+    skillsSlide.style.left = 0;
 }
-
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-    });
-}
-
-topButton.addEventListener('click', scrollToTop);
-
 
   const menu = document.querySelector('.menu');
 
@@ -27,7 +72,6 @@ topButton.addEventListener('click', scrollToTop);
 
   window.addEventListener("scroll", (e) => {
     console.log(scrollY)
-    if(window.scrollY > 190){
-        skillsSlide.style.left = 0;
-    }
+    if(window.scrollY > 190) skillsSlide.style.left = 0;
 });
+
